3743d9af06eabe3dd849db97793a2f10eed9be9b
olegponyat <113192651+olegponyat@users.noreply.github.com>
2024-01-16 08:42:31 -0500

    Update lafart.js

--- lafart.js
+++ lafart.js
@@ -1,18 +1,17 @@
 const DOMSelectors = {
 
 }
-const URL = 'https://opentdb.com/api.php?amount=10&type=multiple'
+function getRandomInt() {
+    return Math.floor(Math.random() * (32 - 9 + 1) + 9);
+}
+const URL = `https://opentdb.com/api.php?amount=10&category=${getRandomInt()}&type=multiple`
 async function getData(URL){
     
     try{
         let response = await fetch(URL)
         let awaitJSON = await response.json()
         console.log(awaitJSON)
-        function getRandomInt(min, max) {
-            min = Math.ceil(min);
-            max = Math.floor(max);
-            return Math.floor(Math.random() * (max - min) + min);
-        }
+        
         awaitJSON.results.forEach((el)=>{
             console.log(el.question,el.correct_answer)
         })
