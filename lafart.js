af5ff549272071c84c13c5b3e3e42cb8f1c38ffd
olegponyat <113192651+olegponyat@users.noreply.github.com>
2024-01-30 23:38:08 -0500

    t

--- lafart.js
+++ lafart.js
@@ -1,7 +1,9 @@
-//let gamePlay = 'y'
-//while(gamePlay === 'y'){
+let gamePlay = 'y'
+let numberCorrect = 0
+while(gamePlay === 'y'){
     let userAnswers = []
     let correctAnswers = []
+
     function getRandomInt() {
         return Math.floor(Math.random() * (32 - 9 + 1) + 9);
     }
@@ -12,7 +14,6 @@
             let awaitJSON = await response.json()
             console.log(awaitJSON)
             for(i=0; i < awaitJSON.results.length; i++){
-
                 let arr = []
                 awaitJSON.results[i].incorrect_answers.forEach((answer)=> arr.push(answer))
                 arr.push(awaitJSON.results[i].correct_answer)
@@ -20,34 +21,32 @@
                     array.sort(() => Math.random() - .50);
                 }
                 shuffle(arr)
-                function setAnswers(){
-                    for (let i = 0; i < Math.min(randomWordsArray.length, 4); i++) {
-                        let currentLetter = String.fromCharCode('a'.charCodeAt(0) + i);
-                        arr[i] = currentLetter + ': ' + randomWordsArray[i];
-                    }
-                }
-                setAnswers()
-                let userAns = prompt(`${awaitJSON.results[i].question}`)
+                let userAns = prompt(`${awaitJSON.results[i].question}, ${arr}`)
                 function getWordByLetter(letter) {
                     const letterMapping = {
                       'A': 0,
                       'B': 1,
                       'C': 2,
                       'D': 3
-                    };
-                  
+                    };                  
                     const index = letterMapping[letter.toUpperCase()];
                     return arr[index];
                 }
                 userAnswers.push(getWordByLetter(userAns))
-                correctAnswers.push(awaitJSON.results[i].correct_answer)
-                
+                correctAnswers.push(awaitJSON.results[i].correct_answer)   
             }
-            console.log(userAnswers,correctAnswers)
-            //gamePlay === 'n'
+            
+            for(i=0;i < 9; i++){
+                if(userAnswers[i] === correctAnswers[i]){
+                    numberCorrect++
+                }else{
+                    numberCorrect
+                }
+            }
         }catch(error){
-
+            console.log(error)
         }
     }
     getData(URL)
-//}
+
+}
