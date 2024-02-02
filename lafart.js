a4cd37e34767a3d3a255924cd8ee12c03efb3da9
olegponyat <113192651+olegponyat@users.noreply.github.com>
2024-02-01 22:28:35 -0500

    m

--- lafart.js
+++ lafart.js
@@ -1,69 +1,82 @@
-let gamePlay = 'y'
-let numberCorrect = 0
-//while(gamePlay = 'y'){
-    function main(){
-        let userAnswers = []
-        let correctAnswers = []
+let gamePlay = 'y';
+let numberCorrect = 0;
 
-        function getRandomInt() {
-            return Math.floor(Math.random() * (32 - 9 + 1) + 9);
-        }
-        const URL = `https://opentdb.com/api.php?amount=10&category=${getRandomInt()}&type=multiple`
-        async function getData(URL){  
-            try{
-                let response = await fetch(URL)
-                let awaitJSON = await response.json()
-                console.log(awaitJSON)
-                for(i=0; i < awaitJSON.results.length; i++){
-                    let arr = []
-                    awaitJSON.results[i].incorrect_answers.forEach((answer)=> arr.push(answer))
-                    arr.push(awaitJSON.results[i].correct_answer)
-                    function shuffle(array){
-                        array.sort(() => Math.random() - .50);
-                    }
-                    shuffle(arr)
-                    
-                    let questionAnswered = false
-                    while(questionAnswered === false){
-                        let userAns = prompt(`${awaitJSON.results[i].question}, \n\n ${arr}`).toUpperCase()
-                        console.log(userAns)
-                        if(userAns != 'A' && userAns != 'B' && userAns != 'C' && userAns != 'D'){
-                            alert('not a letter you can pick. A, B, C, or D')
-                            continue
-                        }else {
-                            function getWordByLetter(letter) {
-                                const letterMapping = {
-                                'A': 0,
-                                'B': 1,
-                                'C': 2,
-                                'D': 3
-                                };                  
-                                const index = letterMapping[letter.toUpperCase()];
-                                return arr[index];
-                            }
-                            userAnswers.push(getWordByLetter(userAns))
-                            correctAnswers.push(awaitJSON.results[i].correct_answer)   
-                            questionAnswered === true
-                            break
-                        }
-                    }
-                }
-                
-                for(i=0;i <= 9; i++){
-                    if(userAnswers[i] === correctAnswers[i]){
-                        numberCorrect++
-                    }else{
-                        numberCorrect
-                    }
-                }
-                console.log(numberCorrect,userAnswers,correctAnswers)
-                
-            }catch(error){
-                console.log(error)
-            }
-        }
-        getData(URL)
+async function main() {
+  while (gamePlay === 'y') {
+    let userAnswers = [];
+    let correctAnswers = [];
+    function getRandomInt() {
+      return Math.floor(Math.random() * (32 - 9 + 1) + 9);
     }
-    main()
+
+    const URL = `https://opentdb.com/api.php?amount=10&category=${getRandomInt()}&type=multiple`;
     
-//}
+    async function getData(URL) {
+      try {
+        let response = await fetch(URL);
+        let awaitJSON = await response.json();
+        console.log(awaitJSON)
+        alert(`Welcome. You will be assigned 10 questions to solve based on a randomized category. The difficulty will automatically be set to easy. Pick your answers based on A, B, C, or D. Your category is: ${awaitJSON.results[0].category}`)
+        for (let i = 0; i < awaitJSON.results.length; i++) {
+          let arr = [];
+          awaitJSON.results[i].incorrect_answers.forEach((answer) => arr.push(answer));
+          arr.push(awaitJSON.results[i].correct_answer);
+
+          function shuffle(array) {
+            array.sort(() => Math.random() - 0.5);
+          }
+          shuffle(arr);
+          let questionAnswered = false;
+          while (!questionAnswered) {
+            let userAns = prompt(`${awaitJSON.results[i].question}, \n\n ${arr}`).toUpperCase();
+            if (userAns != 'A' && userAns != 'B' && userAns != 'C' && userAns != 'D') {
+              alert('Not a valid letter. Please choose A, B, C, or D.');
+              continue;
+            } else {
+              function getWordByLetter(letter) {
+                const letterMapping = {
+                  'A': 0,
+                  'B': 1,
+                  'C': 2,
+                  'D': 3
+                };
+                const index = letterMapping[letter.toUpperCase()];
+                return arr[index];
+              }
+
+              userAnswers.push(getWordByLetter(userAns));
+              correctAnswers.push(awaitJSON.results[i].correct_answer);
+              questionAnswered = true;
+              break;
+            }
+          }
+        }
+
+        for (let i = 0; i <= 9; i++) {
+          if (userAnswers[i] === correctAnswers[i]) {
+            numberCorrect++;
+          } else {
+            numberCorrect
+          }
+        }
+        alert(`You got a total of ${numberCorrect} right out of 10. Cool beans`)
+      } catch (error) {
+        throw new Error(error)
+      }
+    }
+
+    await getData(URL);
+
+    let fart = prompt('Do you want to play again? (y or n)').toLowerCase();
+    if (fart === 'y') {
+      gamePlay = 'y';
+    } else {
+      gamePlay = 'n';
+      alert('You have ended the game. Goodbye!!!!')
+    }
+  }
+}
+
+if (gamePlay === 'y') {
+  main();
+}
